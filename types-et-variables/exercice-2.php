c85e44c30a8ae85d4668f99753a7dc9c926ab451
gaelfelix <gael.felix@icloud.com>
2024-05-15 08:34:46 +0000

    exercice 2 types et variables

---
+++ types-et-variables/exercice-2.php
@@ -0,0 +1,9 @@
+<?php 
+    $nb_tab = [17, 17.1, 17.2, 17.3, 17.4, 17.5];
+    print_r ($nb_tab);
+    echo "<br>";
+    
+    $nb_tab = ["Chat", "Chien", "Poule", "Lapin", "Darmanin"];
+    print_r ($nb_tab);
+    echo "<br>";
+?>
